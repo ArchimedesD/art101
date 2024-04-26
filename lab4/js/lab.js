548b46969ac33389f3eef2faeac66293b3b8b7b9
ArchimedesD <addickso@ucsc.edu>
2024-04-26 00:52:46 -0700

    pseudocode

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,9 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Snake Game Implementation
+// Author: Archie Dickson <addickso@ucsc.edu>
+// Date: 4/23/2024
+
+//Print board
+  //Spawn snake and food at specific coordinates on board	
+  //Create walls
+  //Set snake directed towards right
+  //Display score
+
+//Commence game upon user input 
+  //Accept user input to be able to move snake around in all 4 directions 
+  //Include collision with the player and the borders
+  //Evaluate player growth upon consuming food 
+    //Display score increasing by one unit upon consumption
+  //Randomize food spawn after consumption
+
+//Lose Game
+  //Display final score and option to restart the game after death
 
 // Constants
-
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
